1f69ea665cc505853455219f526708acee53b6f3
William Tait <William@tharcias-air.home>
2023-04-11 00:41:08 +0100

    wip

--- api.py
+++ api.py
@@ -1,102 +1,115 @@
 """
 Main module of the book API.
 """
-from typing import List, Optional
+from typing import Optional
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
 
 app = FastAPI()
 
 
 class Book(BaseModel):
     """
-    Book model, with the fields id, title, author, release_year, ISBN, publisher,
+    Book model, with the fields id, title, author, release_year, isbn, publisher,
     genre, format, and description.
     """
     id: int
     title: str
     author: str
     release_year: int
-    ISBN: str
+    isbn: str
     publisher: Optional[str] = None
     genre: Optional[str] = None
     format: Optional[str] = None
     description: Optional[str] = None
 
 
-# Add book objects
-books = [
-    Book(id=1,
-         title='Flamengo',
-         author='Zico',
-         release_year=2019,
-         ISBN="100100100",
-         publisher="CBF",
-         genre="Futebol",
-         format="Hardcover",
-         description="Em dezembro de 81"),
-    Book(id=2,
-         title='Corinthians',
-         author='Socrates',
-         release_year=2000,
-         ISBN="-10001010",
-         publisher="CBF",
-         genre="Futebol",
-         format="Paperback",
-         description="Democracia corinthiana")
-]
+"""
+Connecting to PostgreSQL database
+"""
+try:
+    conn = psycopg2.connect(host='localhost', database='books-crud',
+                            user='postgres', password='Password1234', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print('Database connection was succesfull!')
+except Exception as error:
+    print('Connecting to database failed')
+    print('Error:', error)
 
 
-@app.get("/books", response_model=List[Book])
-async def get_books():
+# GET all books
+@app.get("/books")
+def get_books():
     # Retrieve all books.
-    return books
+    cursor.execute(""" SELECT * FROM books """)
+    books = cursor.fetchall()
+
+    return {"data": books}
 
 
+# ADD a book
 @app.post("/add_book", status_code=status.HTTP_201_CREATED)
-async def add_book(book: Book):
-    # Add a new book to the collection.
-    books.append(book)
+def add_book(book: Book):
+    cursor.execute(
+        """ INSERT INTO books (id, title, author, release_year, isbn) VALUES (%(id)s, %(title)s, %(author)s, %(release_year)s, %(isbn)s) RETURNING * """, book.dict())
+    cursor.fetchone()
+    conn.commit()
+
     return {"message": "Book added successfully!"}
 
 
-@app.get("/books/id/{book_id}")
-async def get_book_by_id(book_id: int):
+# GET book by id
+@ app.get("/books/id/{id}")
+def get_book_by_id(id: int):
     # Retrieve a book by ID.
-    for book in books:
-        if book.id == book_id:
-            return book
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+    cursor.execute(""" SELECT * from books WHERE id = %s """, (id,))
+    book_by_id = cursor.fetchone()
+    if not book_by_id:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"book with id: {id} was not found")
+
+    return {"data": book_by_id}
 
 
-@app.get("/books/author/{author}")
-async def get_book_by_author(author: str):
-    # Retrieve a book by author name.
-    for book in books:
-        if book.author == author:
-            return book
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="author not found")
+# GET book by author
+@ app.get("/books/author/{author}")
+def get_book_by_author(author: str):
+    cursor.execute(""" SELECT * from books WHERE author = %s """, (author,))
+    book_by_author = cursor.fetchone()
+    if not book_by_author:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"book with author: {author} was not found")
+
+    return {"data": book_by_author}
 
 
-@app.delete("/books/{book_id}")
-async def del_book_by_id(book_id: int):
-    # Delete a book by ID.
-    for book in books[:]:
-        if book.id == book_id:
-            books.remove(book)
-            return {"message": f"Book id {book_id} deleted successfully."}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+# DELETE a book by id
+@ app.delete("/books/{id}")
+def del_book_by_id(id: int):
+    cursor.execute(""" DELETE FROM books WHERE id = %s """, (id,))
+    conn.commit()
+
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"book with id: {id} was not found")
+
+    return {"message": "Book was successfully deleted!"}
 
 
-@app.put("/books/{book_id}")
-async def update_book(book_id: int, book: Book):
-    # Update a book by ID.
-    for idx, book_obj in enumerate(books):
-        if book_obj.id == book_id:
-            books[idx] = book
-            return {"message": f"Book with id {book_id} updated successfully"}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail="id not found")
+# UPDATE a book by id
+@ app.put("/books/{id}")
+def update_book(id: int, book: Book):
+    cursor.execute(""" UPDATE books SET id = %s, title = %s, author = %s, release_year = %s, isbn = %s, publisher = %s, genre = %s, format = %s, description = %s WHERE id = %s RETURNING * """,
+                   (book.id, book.title, book.author, book.release_year, book.isbn, book.publisher, book.genre, book.format, book.description, id,))
+
+    updated_book = cursor.fetchone()
+    conn.commit()
+
+    if updated_book is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"book with id: {id} not found")
+
+    return {"message": f"Book with id {id} updated successfully"}
--- test_api.py
+++ test_api.py
@@ -1,39 +1,49 @@
 from fastapi.testclient import TestClient
-from api import app
+from api import app, add_book
 
 
 client = TestClient(app)
 
 
 def test_get_all_books():
+    # CREATE 2 new books
+    new_book_1 = {
+        "id": 1,
+        "title": "Flamengo",
+        "author": "Zico",
+        "release_year": 1981,
+        "isbn": "FLA1981",
+    }
+    add_book(new_book_1)
+    response = client.post("/add_book", json=new_book_1)
     response = client.get("/books")
     assert response.status_code == 200
-    assert len(response.json()) == 2
+    assert len(response.json()["data"]) > 0
 
 
 def test_get_book_by_id():
     response = client.get("/books/id/1")
     assert response.status_code == 200
-    assert response.json()["title"] == "Flamengo"
+    assert response.json()["data"]["id"] == 1
 
 
 def test_get_book_by_author():
     response = client.get("/books/author/Zico")
     assert response.status_code == 200
-    assert response.json()["id"] == 1
+    assert response.json()["data"]["author"] == "Zico"
 
 
 def test_add_book():
     new_book = {
-        "id": 3,
-        "title": "São Paulo",
-        "author": "Raí",
-        "release_year": 2005,
-        "ISBN": "201202002",
+        "id": 30,
+        "title": "Gremio",
+        "author": "Renato",
+        "release_year": 2000,
+        "isbn": "GR123456789",
         "publisher": "CBF",
         "genre": "Futebol",
         "format": "Paperback",
-        "description": "Campeão do mundo em 2005"
+        "description": "Cincum"
     }
     response = client.post("/add_book", json=new_book)
     assert response.status_code == 201
@@ -41,9 +51,9 @@ def test_add_book():
 
 
 def test_delete_book_by_id():
-    response = client.delete("/books/1")
+    response = client.delete("/books/3")
     assert response.status_code == 200
-    assert response.json()["message"] == "Book id 1 deleted successfully."
+    assert response.json()["message"] == "Book was successfully deleted!"
 
 
 def test_update_book():
@@ -52,7 +62,7 @@ def test_update_book():
         "title": "Corinthians",
         "author": "Socrates",
         "release_year": 2010,
-        "ISBN": "-10001010",
+        "isbn": "-10001010",
         "publisher": "CBF",
         "genre": "Futebol",
         "format": "Paperback",
